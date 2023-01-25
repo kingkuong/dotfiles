2d76db1cc74c3df8d4f94dd0628d042fed4d3774
Cuong Tran <cuongtran92@gmail.com>
2023-01-25 16:12:23 -0500

    add docker user

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,8 @@ FROM ubuntu:focal
 ARG TAGS
 WORKDIR /usr/local/bin
 RUN apt update
-RUN apt install -y curl git ansible build-essential
+RUN apt install -y curl git ansible build-essential sudo
+RUN useradd -m docker && echo "docker:password" | chpasswd && adduser docker sudo
+USER docker
 COPY . .
+CMD /bin/bash
---
+++ vim/setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+cd ~/vim
+
+sudo ./configure --with-features=huge \
+    --enable-multibyte \
+    --enable-rubyinterp=yes \
+    --enable-python3interp=yes \
+    --with-python3-config-dir=$(python3-config --configdir) \
+    --enable-perlinterp=yes \
+    --enable-luainterp=yes \
+    --enable-gui=gtk2 \
+    --enable-cscope \
+    --prefix=/usr/local
+
+sudo make install
